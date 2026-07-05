719b8d4a816cba5a9ab9b0a10e269ffcabb35cc1
Your Name <hel-yamo@c4r1p7.1337.ma>
2026-07-05 11:47:10 +0100

    length fLongest Substring

--- Longest Substring/Longest_substring.py
+++ Longest Substring/Longest_substring.py
@@ -1,22 +1,64 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         count = 0
+#         no_dup = ""
+
+#         for c in s:
+#             if c in no_dup:
+#                 no_dup = no_dup[: -count + 1]
+#                 count = 0
+
+#             elif c not in no_dup:
+#                 # print(no_dup)
+#                 no_dup += c
+#                 count += 1
+
+#         print(no_dup)
+#         return len(no_dup) ##66
+
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         count = 0
+#         no_dup = ""
+
+#         for c in s:
+#             if c in no_dup:
+#                 idx = no_dup.index(c)
+#                 no_dup = no_dup[idx + 1 :]
+#             no_dup += c
+
+#             if c not in no_dup:
+#                 # print(no_dup)
+#                 print("e")
+#                 no_dup += c
+#                 count += 1
+
+#         print(no_dup)
+#        return count     ###83
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        count = 0
+        count_max = 0
         no_dup = ""
-        ind = 0
 
         for c in s:
-            print(c)
             if c in no_dup:
-                print(no_dup)
-                no_dup = no_dup[-count:]
-                count = 0
+                idx = no_dup.index(c)
+                no_dup = no_dup[idx + 1:]
+                # no_dup += c
 
-            elif c not in no_dup:
+            if c not in no_dup:
                 # print(no_dup)
                 no_dup += c
-                count += 1
 
-        return count
+            if len(no_dup) > count_max:
+                # print("entr")
+                count_max = len(no_dup)
+
+        # print(f"h{len(no_dup)}h")
+        return count_max
 
 
 s = "abcabcbb"
